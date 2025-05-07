1f562af5f9387ea95cf799a5b35cbcce674c4df0
jasim-786 <jasimsagheer786@gmail.com>
2025-05-07 15:59:03 +0500

    Sidebar added in CropDiaryScreen.

--- lib/CropDiaryScreen.dart
+++ lib/CropDiaryScreen.dart
@@ -1,5 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_1/AboutUsScreen.dart';
+import 'package:flutter_application_1/LoginScreen.dart';
+import 'package:flutter_application_1/Onboarding1.dart';
+import 'package:flutter_application_1/PreHomeScreen.dart';
+import 'package:flutter_application_1/PreviousResultsScreen.dart';
+import 'package:flutter_application_1/ProfileScreen.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
@@ -223,6 +231,49 @@ class _CropDiaryScreenState extends State<CropDiaryScreen>
 
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+
+  User? user = FirebaseAuth.instance.currentUser;
+  void logout() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Logout"),
+          content: Text("Are you sure you want to log out?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close dialog
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context); // Close dialog
+
+                await FirebaseAuth.instance.signOut();
+
+                // Show logout success message
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Logged out successfully"),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+
+                // Navigate back to login screen
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: Text("Logout", style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   void initState() {
@@ -1564,6 +1615,121 @@ class _CropDiaryScreenState extends State<CropDiaryScreen>
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          color: Color(0xFFE5D188), // Light yellow background
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  // Top Section with Background Image
+                  Container(
+                    height: screenHeight * 0.25,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/Sidebar_Top.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20), // Spacing
+
+                  // Sidebar Buttons
+                  buildSidebarButton(
+                    customIconPath: "assets/icons/Home_icon.png",
+                    text: 'Home'.tr(),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PreHomeScreen()),
+                      );
+                    },
+                  ),
+                  buildSidebarButton(
+                    customIconPath: "assets/icons/profile_icon.png",
+                    text: 'Profile'.tr(),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen()),
+                      );
+                    },
+                  ),
+                  buildSidebarButton(
+                    customIconPath: "assets/icons/history_icon.png",
+                    text: 'History'.tr(),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PreviousResultsScreen()),
+                      );
+                    },
+                  ),
+                  buildSidebarButton(
+                    customIconPath: "assets/icons/help_icon.png",
+                    text: 'Help'.tr(),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Onboarding1()),
+                      );
+                    },
+                  ),
+                  buildSidebarButton(
+                    customIconPath: "assets/icons/feedback_icon.png",
+                    text: 'Feedback'.tr(),
+                    onTap: () {
+                      // Handle Profile Navigation
+                    },
+                  ),
+                  buildSidebarButton(
+                    customIconPath: "assets/icons/info_icon.png",
+                    text: 'About Us'.tr(),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AboutUsScreen()),
+                      );
+                    },
+                  ),
+                  Column(
+                    children: [
+                      if (user != null)
+                        buildSidebarButton(
+                          customIconPath: "assets/icons/logout_icon.png",
+                          text: 'Logout'.tr(),
+                          onTap: () {
+                            logout();
+                          },
+                        ),
+                    ],
+                  ),
+                ],
+              ),
+
+              // Logo Positioned Below Top Section
+              Positioned(
+                top: screenHeight * 0.1, // Adjust for desired position
+                left: 0,
+                right: 140,
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    height: 140, // Adjust size as needed
+                    width: 140, // Adjust size as needed
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Stack(
         children: [
           Container(
@@ -1728,6 +1894,68 @@ class _CropDiaryScreenState extends State<CropDiaryScreen>
           elevation: 6,
           child: const Icon(Icons.grass, color: AppConstants.primaryColor),
           tooltip: 'Add Entry',
+        ),
+      ),
+    );
+  }
+
+  /// Custom Sidebar Button
+  Widget buildSidebarButton({
+    IconData? icon,
+    String? customIconPath,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(vertical: 6, horizontal: 15), // Button Spacing
+      child: GestureDetector(
+        onTap: onTap,
+        child: Transform.translate(
+          offset: Offset(-10, 0), // Move button slightly left
+          child: Container(
+            height: 64,
+            width: 250,
+            decoration: BoxDecoration(
+              color: Color(0xFF7B5228), // Brown background for button
+              borderRadius: BorderRadius.circular(30), // Rounded button shape
+            ),
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            child: Row(
+              children: [
+                // Circular icon background
+                Transform.translate(
+                  offset: Offset(-8, 0), // Moves the icon slightly left
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFE5D188), // Light background for icon
+                      shape: BoxShape.circle,
+                    ),
+                    padding:
+                        EdgeInsets.all(10), // Adjust for proper icon placement
+                    child: customIconPath != null
+                        ? Image.asset(
+                            customIconPath,
+                            height: 26,
+                            width: 26,
+                          )
+                        : Icon(icon, color: Colors.black, size: 24),
+                  ),
+                ),
+                SizedBox(width: 10), // Space between icon and text
+
+                // Profile text
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
